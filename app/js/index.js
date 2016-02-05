6c08e6d773f8b7d47a98f46861d0ecc8226a4f53
kotazi <kotazi@kotazi.com>
2016-02-05 21:09:34 +0900

    Add vue-resources

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,29 @@
+
+var Vue = require('vue');
+Vue.use(require('vue-resource'));
+var data = require("./models/data.js");
+
 require("./../css/style.css");
 
 
 $(document).ready(function() {
 
   var CuHeaderComponent = Vue.extend({
-    template: '<h1>Hello, Calendar!!</h1>'
+    template: '<h1>Hello, Calendar!!<br>{{message}}</h1>',
+    data: {
+      message: 'hoge'
+    },
+    ready: function() {
+      this.$http({url: 'http://echo.jsontest.com/key/the-text-got-by-json', method: 'GET'}).then(function (response) {
+          // success callback
+          if (!response.data) {
+            return false;
+          }
+          this.$set('message', response.data.key)
+      }, function (response) {
+          // error callback
+      });
+    }
   });
 
   Vue.component('cu-header', CuHeaderComponent);
@@ -15,9 +34,7 @@ $(document).ready(function() {
 
   var externalEventsVM = new Vue({
     el: '#external-events',
-    data: {
-      items: 6
-    },
+    data: data,
     ready: function() {
       $('#external-events .fc-event').each(function() {
         $(this).data('event', {
